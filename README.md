1529424c0864cc38fd984a5f08f2435c63b9cc8f
Henrik Oldenborg <henrik.oldenborg@lego.com>
2019-10-15 11:15:50 +0200

    Enable async await within the module

--- README.md
+++ README.md
@@ -5,7 +5,45 @@
 [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
 [![Ask Me Anything !](https://img.shields.io/badge/Ask%20me-anything-1abc9c.svg)](https://github.com/oldenborg/node-grep/issues/new)
 
-
-
 # node-grep
-Missing grep tool for node
+Want a tool for node that works like grep. Here you go.
+
+## The reason for making this
+I wanted a tool that I could use in my node applications that works like grep and is just as fast.
+
+## install
+```bash
+npm install node-grep
+```
+
+## Basic usage
+```js
+const grep = require('node-grep');
+
+grep('translate','./src');
+```
+
+## Output
+```js
+{
+  term: 'translate',
+  count: 3,
+  matches: [
+    {
+      file: './src/first-file.txt'
+      match: 'I want to translate this line, but I have',
+      line: "23",
+    },
+    {
+      file: './src/first-file.txt'
+      match: 'and then he said, can you translate it for me?',
+      line: "27",
+    },
+    {
+      file: './src/another-file.js'
+      match: 'const translation = translate("about.introduction.title");',
+      line: "144",
+    }
+  ]
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,5 +1,60 @@
 const fs = require('fs');
+const path = require('path');
 
+
+async function grep(term, dir) {
+  if(!term) {
+    throw "A search term is required";
+  }
+
+  if(!dir) {
+    dir = process.cwd();
+  }
+
+  if(typeof term !== "string") {
+    throw "Search term must be a string";
+  }
+
+  dir = path.resolve(dir);
+
+  if(!fs.existsSync(dir)) return;
+
+/*
+  const start = fs.readdirSync(dir);
+  const matches = resolveFiles(dir);
+
+  const items = await fs.readdirSync(dir);
+
+  if(items.length === 0) return;
+  for (item in items) {
+    const itemPath = path.resolve(dir, items[item]);
+    if(fs.statSync(itemPath).isDirectory()) {
+      await resolveFiles(itemPath);
+    } else {
+      console.log(itemPath);
+    }
+  }
+*/
+  return {
+    dir,
+    term,
+    count: 0,
+    matches: [
+      {
+        file: 'path/to/fake/dir/some-file.txt',
+        match: 'Some example content here',
+        line: 1
+      }
+    ]
+  }
+}
+
+module.exports.grep = grep;
+/*
+
+const resolveFiles = async dir => {
+
+}
 module.exports = (term, dir) => {
 
   if(!term) {
@@ -16,9 +71,21 @@ module.exports = (term, dir) => {
 
   dir = path.resolve(dir);
 
+  const start = fs.readdirSync(dir);
+
+  const matches = resolveFiles(dir);
+
   return {
     dir,
     term,
-    count: 0
+    count: 0,
+    matches: [
+      {
+        file: 'path/to/fake/dir/some-file.txt',
+        match: 'Some example content here',
+        line: 1
+      }
+    ]
   }
 }
+*/
